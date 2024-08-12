738953ebc57d0f32c8453fb0d4651a6ea2057575
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-08-12 01:37:54 -0600

    Update page

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Importar tus componentes desde node_modules
+import "@luisenriquechavarriavazquez/my-button/index";
+import "@luisenriquechavarriavazquez/my-text-box/index";
+
+// Código adicional que puedas necesitar para inicializar la aplicación
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Aplicación cargada y lista.");
+});
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Asegúrate de que el punto de entrada sea correcto
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'production'
+};
