7b66160ad0b8fc3c5c18368625deee47d5a1d616
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-08-12 01:55:31 -0600

    Prueba format currency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Importar tus componentes desde node_modules
 import "@luisenriquechavarriavazquez/my-button/index";
 import "@luisenriquechavarriavazquez/my-text-box/index";
+import "@luisenriquechavarriavazquez/currency-format/index";
 
 // Código adicional que puedas necesitar para inicializar la aplicación
 document.addEventListener("DOMContentLoaded", () => {
