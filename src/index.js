30ee341c1aeb6a09c13d1433e51b76d059a72b2f
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-09-01 16:27:45 -0600

    New design and new components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 // Importar tus componentes desde node_modules
-import "@luisenriquechavarriavazquez/bpt-currency-format/index";
-import "@luisenriquechavarriavazquez/bpt-stepper/index";
-import "@luisenriquechavarriavazquez/bpt-modal/index";
-import "@luisenriquechavarriavazquez/bpt-button-modal/index";
 import "@luisenriquechavarriavazquez/bpt-sidebar/index";
+import "@luisenriquechavarriavazquez/bpt-badge/index";
+import "@luisenriquechavarriavazquez/bpt-button/index";
+import "@luisenriquechavarriavazquez/bpt-button-modal/index";
+import "@luisenriquechavarriavazquez/bpt-card-basic/index";
+import "@luisenriquechavarriavazquez/bpt-card-image/index";
+import "@luisenriquechavarriavazquez/bpt-chips/index";
+import "@luisenriquechavarriavazquez/bpt-currency-format/index";
+import "@luisenriquechavarriavazquez/bpt-icon-button/index";
+import "@luisenriquechavarriavazquez/bpt-modal/index";
+import "@luisenriquechavarriavazquez/bpt-stepper/index";
 
 // Código adicional que puedas necesitar para inicializar la aplicación
 document.addEventListener("DOMContentLoaded", () => {
